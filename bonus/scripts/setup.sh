d559f0d35cc3c7c06e66fc8c06cdf2e979e2b3d5
iassafe <ikrameassafe17@gmail.com>
2026-07-07 17:00:34 +0000

    done

--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -44,8 +44,8 @@ ok "Namespaces ready"
 
 # [3/7] Argo CD
 info "Installing Argo CD..."
-kubectl apply -n argocd \
-    -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml || true
+kubectl apply --server-side -n argocd \
+    -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
 ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret \
     -o jsonpath="{.data.password}" | base64 -d)
